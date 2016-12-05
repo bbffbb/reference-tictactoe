b05a9702741d4d63d321fb1f668f907db0b6bc52
bbffbb <birkirfb13@ru.is>
2016-12-05 22:40:58 +0000

    ..

--- productionBuild.sh
+++ productionBuild.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-npm install --silent
-cd client 
-npm install --silent
-cd .. 
+npm install 
+cd ./client
+npm install 
+cd ..
 #clean and build the production build
-npm run build 
+npm run build
 
 #copy important files to the build dir
 cp ./Dockerfile ./build/
@@ -12,10 +12,9 @@ cp ./package.json ./build/
 cp ./run ./build/
 
 #navigate into the build for the docker build to create the container from build dir
+cd ./build
 
 
 
 #build the image
-docker build -t birkirfb/tictactoe . 
 
-docker push birkirfb/tictactoe 
