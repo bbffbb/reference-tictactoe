2e25a6913f6a77681f0495f3f88cf1e3acdb4c5b
bbffbb <birkirfb13@ru.is>
2016-12-06 11:40:02 +0000

    GameJoined & FullGameJoinEvent successful

--- server/socket-app/tictactoe/tictactoe-game.spec.js
+++ server/socket-app/tictactoe/tictactoe-game.spec.js
@@ -125,10 +125,54 @@ describe('join game command', function () {
 
     });
 
-    it('should emit FullGameJoinAttempted event when game full..implement this', function () {
+    it('should emit FullGameJoinAttempted event when game full..', function () {
 
-        expect(true).toBe(false);
+        given = [
+            {
+                type: "GameCreated",
+                user: {
+                    userName: "TheGuy"
+                },
+                name: "TheFirstGame",
+                timeStamp: "2014-12-02T11:29:29"
+            },
+            {
+                type: "GameJoined",
+                user: {
+                    userName: "Gummi"
+                },
+                name: "TheFirstGame",
+                timeStamp: "2014-12-02T11:29:29"
+            }
+        ];
+        
+        when = 
+        {
+            type: "JoinGame",
+            user: {
+                userName: "Birkir"
+            },
+            name: "TheFirstGame",
+            timeStamp: "2014-12-02T11:30:29"
+        };
+
+        then = [
+        {
+            type: "FullGameJoinAttempted",
+            user: {
+                userName: "Birkir"
+            },
+            name: "TheFirstGame",
+            timeStamp: "2014-12-02T11:30:29"
+        }];
+
     });
+
+
+
+
+
+
 });
 
 
