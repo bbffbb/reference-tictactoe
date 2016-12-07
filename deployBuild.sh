9717b803fb8269b838800f13e1c6b9f2640eab49
bbffbb <birkirfb13@ru.is>
2016-12-07 16:34:17 +0000

    deploy script to AWS testing

--- deployBuild.sh
+++ deployBuild.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 
-scp -o StrictHostKeyChecking=no ~/Desktop/my-ec2-key-pair.pem ubuntu@82.221.49.114:~/my-ec2-key-pair.pem
-
-
-scp -o StrictHostKeyChecking=no -i "~/my-ec2-key-pair.pem" ./docker-compose.yml ec2-user@ec2-184-72-202-240.compute-1.amazonaws.com:~/docker-compose.yml
-scp -o StrictHostKeyChecking=no -i "~/my-ec2-key-pair.pem" ./provision/compose-and-run.sh ec2-user@ec2-184-72-202-240.compute-1.amazonaws.com:~/compose-and-run.sh
+scp -o StrictHostKeyChecking=no -i "~/Desktop/my-ec2-key-pair.pem" ./docker-compose.yml ec2-user@ec2-184-72-202-240.compute-1.amazonaws.com:~/docker-compose.yml
+scp -o StrictHostKeyChecking=no -i "~/Desktop/my-ec2-key-pair.pem" ./provision/compose-and-run.sh ec2-user@ec2-184-72-202-240.compute-1.amazonaws.com:~/compose-and-run.sh
+scp -o StrictHostKeyChecking=no -i "~/Desktop/my-ec2-key-pair.pem" ./build/.env ec2-user@ec2-184-72-202-240.compute-1.amazonaws.com:~/docker-compose.yml
