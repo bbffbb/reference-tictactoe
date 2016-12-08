9d1b22cd7e567877f7f687a00965c550fe506f37
bbffbb <birkirfb13@ru.is>
2016-12-08 17:56:50 +0000

    documentation finished

--- AWS-copy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-scp -o StrictHostKeyChecking=no -i "~/Desktop/my-ec2-key-pair.pem" ./docker-compose.yml ec2-user@ec2-184-72-202-240.compute-1.amazonaws.com:~/docker-compose.yml
-scp -o StrictHostKeyChecking=no -i "~/Desktop/my-ec2-key-pair.pem" ./provision/compose-and-run.sh ec2-user@ec2-184-72-202-240.compute-1.amazonaws.com:~/compose-and-run.sh
--- AWS-login.sh
+++ AWS-login.sh
@@ -1,1 +1,4 @@
-ssh -i ~/Desktop/my-ec2-key-pair ec2-user@ dns
+#!/bin/bash
+
+
+ssh -i ~/Desktop/my-ec2-key-pair ec2-user@ #needs the newly generated public dns from aws console.
