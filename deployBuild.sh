82f2a4feb966d5d1e00f6159e8698bb971846f8d
bbffbb <birkirfb13@ru.is>
2016-12-09 13:14:31 +0000

    day 6 complete

--- deployBuild.sh
+++ deployBuild.sh
@@ -7,7 +7,8 @@ docker pull birkirfb/tictactoe
 #copy script for the aws server. copies important files to run the image on the aws server.
 scp -o StrictHostKeyChecking=no -i "~/my-ec2-key-pair.pem" ./docker-compose.yml ec2-user@ec2-54-234-172-233.compute-1.amazonaws.com:~/docker-compose.yml
 scp -o StrictHostKeyChecking=no -i "~/my-ec2-key-pair.pem" ./provision/compose-and-run.sh ec2-user@ec2-54-234-172-233.compute-1.amazonaws.com:~/compose-and-run.sh
-scp -o StrictHostKeyChecking=no -i "~/my-ec2-key-pair.pem" ./build/.env ec2-user@ec2-54-234-172-233.compute-1.amazonaws.com:~/docker-compose.yml
+
+#scp -o StrictHostKeyChecking=no -i "~/my-ec2-key-pair.pem" ./build/.env ec2-user@ec2-54-234-172-233.compute-1.amazonaws.com:~/docker-compose.yml
 
 
 docker-compose up -d
