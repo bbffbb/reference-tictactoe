87bbd1442ffe1fd31f0da6f55adbfdd8ca9409e2
bbffbb <birkirfb13@ru.is>
2016-12-13 13:28:52 +0000

    typo error

--- deployBuild.sh
+++ deployBuild.sh
@@ -7,7 +7,7 @@ docker pull birkirfb/tictactoe:$GIT_COMMIT
 #copy script for the aws server. copies important files to run the image on the aws server.
 scp -o StrictHostKeyChecking=no -i "~/my-ec2-key-pair.pem" ./docker-compose.yml ec2-user@ec2-54-174-116-245.compute-1.amazonaws.com:~/docker-compose.yml
 scp -o StrictHostKeyChecking=no -i "~/my-ec2-key-pair.pem" ./provision/compose-and-run.sh ec2-user@ec2-54-174-116-245.compute-1.amazonaws.com:~/compose-and-run.sh
-scp -o StrictHostKeyChecking=no -i "~/my-ec2-key-pair.pem" .env ec2-user@ec2-54-174-116-245.compute-1.amazonaws.com:~/docker-compose.yml
+scp -o StrictHostKeyChecking=no -i "~/my-ec2-key-pair.pem" .env ec2-user@ec2-54-174-116-245.compute-1.amazonaws.com:~/.env
 
 
 docker-compose up -d
