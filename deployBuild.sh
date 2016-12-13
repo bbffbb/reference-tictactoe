2c105d0861f63c09f3b834b4302da3e379b9946d
bbffbb <birkirfb13@ru.is>
2016-12-13 11:43:35 +0000

    deploy script correct again

--- deployBuild.sh
+++ deployBuild.sh
@@ -3,7 +3,7 @@
 
 
 docker-compose down
-docker pull birkirfb/tictactoe:$GIT_COMMIT
+docker pull birkirfb/tictactoe
 #copy script for the aws server. copies important files to run the image on the aws server.
 scp -o StrictHostKeyChecking=no -i "~/my-ec2-key-pair.pem" ./docker-compose.yml ec2-user@ec2-54-174-116-245.compute-1.amazonaws.com:~/docker-compose.yml
 scp -o StrictHostKeyChecking=no -i "~/my-ec2-key-pair.pem" ./provision/compose-and-run.sh ec2-user@ec2-54-174-116-245.compute-1.amazonaws.com:~/compose-and-run.sh
